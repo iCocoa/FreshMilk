5a5b54d4a87a37578ac95d116198fa5bd497e605
Hansen Wu <wuhaisheng2015@gmail.com>
2016-06-04 10:49:48 +0800

    Add new Project.

---
+++ FreshMilk/FreshMilk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FreshMilk
+//
+//  Created by Hansen on 16/6/4.
+//  Copyright © 2016年 Hansen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
